f70206ae016d416e9920558f3df0d497ffd9e7c7
ARINDA MARK <enginxp@proton.me>
2023-08-16 19:49:19 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,28 +1,56 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_to_98 - Prints all natural numbers
- * from n to 98.
- *
- * @n: input number.
- *
- * Return: no return.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	if (n > 98)
-	{
-		for (; n > 98; n--)
-		{
-			printf("%d, ", n);
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("%d\n", n);
+    int i,
+        j,
+        k;
+    if (n >= 0 && n <= 15)
+    {
+        for (
+            i = 0;
+            i <= n;
+            i + +)
+        {
+            for (
+                j = 0;
+                j <= n;
+                j + +)
+            {
+                k = j * i;
+                if (j == 0)
+                {
+                    _putchar(k + '0');
+                }
+                else if (k < 10 && j != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(k + '0');
+                }
+                else if (k >= 10 && k < 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar((k / 10) + '0');
+                    _putchar((k % 10) + '0');
+                }
+                else if (k >= 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((k / 100) + '0');
+                    _putchar(((k / 10) % 10) + '0');
+                    _putchar((k % 10) + '0');
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
